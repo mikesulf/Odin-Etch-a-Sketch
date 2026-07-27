b5245f473842ab933fd746a05f60487f69988563
mikesulf <sulfridgew@gmail.com>
2026-07-27 16:19:46 -0400

    okay, added the event listener. That was hard, I still don't fully understand event listener, but https://dev.to/shubhamtiwari909/single-event-listener-for-all-elements-in-js-32o6 helped me get a framework to work with. this console.logs. I still have to get it to change the background-color of the cell to get it to work right.

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
 let x = 16;
 
+
+
 for (i = 0; i < x; i++){
     let divRows = document.createElement('div')
         divRows.classList.add("divRows")
@@ -8,15 +10,19 @@ for (i = 0; i < x; i++){
     for (y = 0; y < x; y++){
         let rowCells = document.createElement('div')
             rowCells.classList.add("rowCells")
+            rowCells.id = "row" + i + "cell" + y
             divRows.appendChild(rowCells)
     }
-}
+};
 
-// // credit to https://stackoverflow.com/questions/3198540/appending-elements-to-dom-in-a-loop-structure
-// let rows = document.getElementsByClassName('divRows');
 
-// for (i = 0; len = rows.len; i<len; i++){
-//     let rowCells = document.createElement('div')
-//         rowCells.classList.add("rowCells")
-//         divRows.appendChild(rowCells)
-// }
+
+//credit for help figuring out how to add attributes to a listen event --> https://stackoverflow.com/questions/8318591/javascript-addeventlistener-using-to-create-a-mouseover-effect
+
+document.querySelector("body").addEventListener("mouseover", (e) => {
+    e.preventDefault();
+
+    e.target.setAttribute("data-toggle-id", true);
+    let cellId = e.target.id
+    console.log(cellId)
+})
